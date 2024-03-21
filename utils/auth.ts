a249b9f5dc8d53a377d26f374f6b43f3dd11c070
Hafizu <piz230601@gmail.com>
2024-03-21 13:01:11 +0800

    change again

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,10 +1,27 @@
+// import NextAuth from "next-auth";
+// import GitHubProvider from "next-auth/providers/github";
+// import type { NextAuthOptions } from "next-auth";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import prisma from "./db";
+
+// export const authOptions = {
+// 	adapter: PrismaAdapter(prisma),
+// 	providers: [
+// 		GitHubProvider({
+// 			clientId: process.env.AUTH_GITHUB_ID as string,
+// 			clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+// 		}),
+// 	],
+// } satisfies NextAuthOptions;
+// export const { handlers, auth, signOut } = NextAuth(authOptions);
+
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import type { NextAuthOptions } from 'next-auth'
+import type { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db";
 
-export const authOptions = {
+const authOptions: NextAuthOptions = {
 	adapter: PrismaAdapter(prisma),
 	providers: [
 		GitHubProvider({
@@ -12,5 +29,6 @@ export const authOptions = {
 			clientSecret: process.env.AUTH_GITHUB_SECRET as string,
 		}),
 	],
-} satisfies NextAuthOptions;
+};
+
 export const { handlers, auth, signOut } = NextAuth(authOptions);
