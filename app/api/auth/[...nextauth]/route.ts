4abb2ded14140a1f0486fe4ecea71de3d1330d27
Hafizu <piz230601@gmail.com>
2024-03-21 13:10:06 +0800

    change the route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,3 @@
-// import { handlers } from "@/utils/auth";
+import { handlers } from "@/utils/auth";
 
-// export const { GET, POST } = handlers;
-
-import type { NextAuthConfig } from "next-auth";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
-import prisma from "@/utils/db";
-import GithubProvider from "next-auth/providers/github";
-
-const handler = NextAuth({
-	providers: [
-		GithubProvider({
-			clientId: process.env.AUTH_GITHUB_ID as string,
-			clientSecret: process.env.AUTH_GITHUB_SECRET as string,
-		}),
-	],
-	adapter: PrismaAdapter(prisma),
-} satisfies NextAuthConfig);
-
-export { handler as GET, handler as POST };
+export const { GET, POST } = handlers;
